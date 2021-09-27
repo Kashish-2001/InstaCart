d4dec8411fc485a6488df7bd59006a91fb6b70f6
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-09-27 12:34:20 +0530

    add navbar and footer

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import logo from 'src/images/logo.jpg'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <div>
+                    <div>
+                        <img src="{logo}" alt=""/>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+// Navbar.defaultProps = {
+//     navTitle : 'InstaCart Navbar'
+// }
+
+export default Navbar
